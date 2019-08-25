d4238bdaccb6bce8fc6d0724ab9d63da93984c18
casianosantana <santana.cms@gmail.com>
2019-08-25 14:26:10 -0300

    ola

---
+++ app.js
@@ -0,0 +1,22 @@
+const koa = require('koa');
+const json = require('koa-json');
+const koaRouter = require('koa-router');
+const app = new koa();
+const router = new koaRouter();
+const koaBody = require("koa-body");
+
+app.use(json());
+//app.use(async ctx => ( ctx.body = {"name":"Zenitsu"}));
+
+router.get("/test",ctx =>(ctx.body = "hello test"));
+router.post("/upload",async ctx=>{
+    const file  = ctx.request.files.file;
+    console.log(file);
+});
+
+app
+.use(router.routes())
+app.use(koaBody({ multipart: true }));
+.use(router.allowedMethods())
+
+app.listen(3000,()=> console.log('Server started...'));
